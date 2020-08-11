c8b5e71dce07a24a7bfbf71014e81f060d008778
zuanming <zuanming@gmail.com>
2020-08-11 15:23:36 +0000

    added courses app

---
+++ courses/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, HttpResponse
+
+# View Functions
+
+def index(request):
+    return HttpResponse("Home")
+
+
+def view_courses(request):
+    return render(request, "courses/view_courses.template.html")
