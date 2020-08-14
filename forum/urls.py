d5e65daef3941cd33b14a21eb5b4b6eba97074fa
zuanming <zuanming@gmail.com>
2020-08-15 01:45:20 +0800

    added create, update and delete for answer

--- forum/urls.py
+++ forum/urls.py
@@ -4,6 +4,10 @@ import forum.views
 
 urlpatterns = [
     path('', forum.views.index, name='view_forum'),
-    path('create/', forum.views.create_question, name='create_question'),
-    path('update/<question_id>', forum.views.update_question, name='update_question')
+    path('question/create/', forum.views.create_question, name='create_question'),
+    path('question/update/<question_id>', forum.views.update_question, name='update_question'),
+    path('question/delete/<question_id>', forum.views.delete_question, name='delete_question'),
+    path('answer/create/<question_id>', forum.views.create_answer, name='create_answer'),
+    path('answer/update/<answer_id>', forum.views.update_answer, name='update_answer'),
+    path('answer/delete/<answer_id>', forum.views.delete_answer, name='delete_answer'),
 ]
--- forum/views.py
+++ forum/views.py
@@ -30,6 +30,7 @@ def create_question(request):
             'question_form':question_form
         })
 
+
 def update_question(request, question_id):
     question_being_updated = get_object_or_404(Question, pk=question_id)
     if request.method=="POST":
@@ -44,4 +45,65 @@ def update_question(request, question_id):
         update_question_form = QuestionForm(instance = question_being_updated)
         return render(request, "forum/update_question.template.html", {
             'update_question_form': update_question_form
+        })
+
+
+def delete_question(request, question_id):
+    question_to_delete = get_object_or_404(Question, pk=question_id)
+    if request.method == 'POST':
+        question_to_delete.delete()
+        messages.success(request, f"Question removed!")
+        return redirect('view_forum')
+    else:
+        return render(request, 'forum/delete_question.template.html', {
+            'question': question_to_delete
+        })
+
+
+def create_answer(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method=="POST":
+        answer_form = AnswerForm(request.POST)
+        answer = answer_form.save(commit=False)
+        answer.user = request.user
+        answer.datetime = datetime.datetime.now()
+        answer.question = question
+        if answer_form.is_valid():
+            answer_form.save()
+            messages.success(request, f"New answer posted!")
+            return redirect('view_forum')
+    else:
+        answer_form = AnswerForm()
+        return render(request, "forum/create_answer.template.html", {
+            'answer_form':answer_form,
+            'question':question,
+        })
+
+
+def update_answer(request, answer_id):
+    answer_being_updated = get_object_or_404(Answer, pk=answer_id)
+    if request.method=="POST":
+        update_answer_form = AnswerForm(request.POST, instance = answer_being_updated)
+        if update_answer_form.is_valid():
+            answer.user = request.user
+            answer.datetime = datetime.datetime.now()
+            answer.save()
+            answer.success(request, f"Answer updated!")
+            return redirect('view_forum')
+    else:
+        update_answer_form = AnswerForm(instance = question_being_updated)
+        return render(request, "forum/update_answer.template.html", {
+            'update_answer_form': update_answer_form
+        })
+
+
+def delete_answer(request, answer_id):
+    answer_to_delete = get_object_or_404(Answer, pk=answer_id)
+    if request.method == 'POST':
+        answer_to_delete.delete()
+        messages.success(request, f"Answer removed!")
+        return redirect('view_forum')
+    else:
+        return render(request, 'forum/delete_answer.template.html', {
+            'answer': answer_to_delete
         })
