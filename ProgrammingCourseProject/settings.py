fc4ed8e5370f96adb9d3ab7ec86ceb8eab77380f
zuanming <zuanming@gmail.com>
2020-08-22 09:21:21 +0000

    added send email from gmail

--- ProgrammingCourseProject/settings.py
+++ ProgrammingCourseProject/settings.py
@@ -97,7 +97,18 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 WSGI_APPLICATION = 'ProgrammingCourseProject.wsgi.application'
 
+if TEST_EMAIL == "1":
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = "smtp.gmail.com"
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASS")
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    DEFAULT_FROM_EMAIL = os.environ.get("EMAIL_HOST_USER")
 
+    
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
