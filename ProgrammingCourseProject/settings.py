f48f704f6ecb095a06f41966a99482b70a149bfb
zuanming <zuanming@gmail.com>
2020-08-25 05:48:56 +0000

    changed scale transformation to translate for course logos. Reinstated link to course logo

--- ProgrammingCourseProject/settings.py
+++ ProgrammingCourseProject/settings.py
@@ -168,6 +168,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 STRIPE_PUBLISHABLE_KEY = os.environ.get('STRIPE_PUBLISHABLE_KEY')
