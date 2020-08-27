90a2c6f858c93a6ec9e4f37986090f58ce10f3b9
zuanming <zuanming@gmail.com>
2020-08-27 14:58:25 +0000

    removed all hosts as allowable hosts

--- ProgrammingCourseProject/settings.py
+++ ProgrammingCourseProject/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*", "programming-course-project.herokuapp.com"]
+ALLOWED_HOSTS = ["programming-course-project.herokuapp.com"]
 
 
 # Application definition
